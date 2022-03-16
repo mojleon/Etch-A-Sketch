d5a7f9094a01c87e6ac67502a8dd0d1cebd9c648
mojleon <damirkalbic@gmail.com>
2022-03-16 12:18:26 +0100

    [PROJECT] Well just made the whole project without frequently uploading. Might do the optional assignment in the future.

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-function construct() {
+function construct(size) {
   let body = document.getElementsByTagName("body");
 
-  let section = document.createElement("section");
-  body[0].appendChild(section);
+  let grid = document.createElement("section");
+  grid.setAttribute("id", "grid");
+  grid.style.cssText = "display: flex;";
+  body[0].appendChild(grid);
 
-  let div = document.createElement("div");
-  div.style.cssText =
-    "width: 16px; height: 16px; background: #333; display: block;";
+  for (let r = 0; r <= size; r++) {
+    let section = document.createElement("section");
+    grid.appendChild(section);
 
-  for (let r = 0; r <= 16; r++) {
-    console.log(r);
-    section.appendChild(div);
+    for (let c = 0; c <= size; c++) {
+      let div = document.createElement("div");
+      div.setAttribute("id", `${r}_${c}`);
+      section.appendChild(div);
+    }
   }
+
+  drawingEventListeners();
 }
 
-construct();
+function drawingEventListeners() {
+  manipulateCanvas("draw");
+}
+
+function draw() {
+  if (window.event.buttons == 1) this.style.backgroundColor = "#00ff00";
+}
+
+function reset() {
+  manipulateCanvas("reset");
+  let iGridSize = window.prompt("How large should the grid new be?");
+
+  if (isNaN(iGridSize)) return;
+  console.log(iGridSize);
+  if (iGridSize <= 100 && iGridSize != null) constructNewGrid(iGridSize);
+}
+
+function constructNewGrid(size) {
+  manipulateCanvas("remove");
+  construct(size);
+}
+
+function manipulateCanvas(action) {
+  let divDrawingBoard = document.getElementsByTagName("div");
+
+  if (action == "reset")
+    [...divDrawingBoard].forEach((div) => {
+      div.style.backgroundColor = "red";
+    });
+  if (action == "draw")
+    [...divDrawingBoard].forEach((div) => {
+      div.addEventListener("mouseover", draw);
+    });
+
+  if (action == "remove")
+    [...divDrawingBoard].forEach((div) => {
+      div.remove();
+    });
+}
+
+construct(40);
